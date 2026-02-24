54161a5d96db22793ce9dcf973f17dd432bd441b
jvchi <elechidestinyj@gmail.com>
2026-02-24 17:17:29 +0100

    first commit

---
+++ components/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='text-red-900 border-b-2 border-black/5 w-full h-16'>
+      <div className='w-full h-max items-center'>
+        <img src="" alt="" />
+        <h1 className=''> IELTS</h1>
+      </div>
+    </div>
+  )
+}
