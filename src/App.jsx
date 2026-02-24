476cbd373d9461068ceec3f458de75e200dd3c60
Muoebonam Chukwuneke <nekeagust@gmail.com>
2026-02-24 17:47:28 +0100

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from '../components/nav'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     <Nav/>
-    </>
-  )
+return (
+  <>
+    <Nav/>
+  </>
+)
 }
 
 export default App
