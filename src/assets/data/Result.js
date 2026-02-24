ae08ae8255aa5c2b450768aafc4c4c51cb93500f
Muoebonam Chukwuneke <nekeagust@gmail.com>
2026-02-24 18:50:05 +0100

    formatted from score from integer to string

--- src/assets/data/Result.js
+++ src/assets/data/Result.js
@@ -2,27 +2,27 @@ const Result = [
     {
         id: 1,
         name: "Speaking",
-        score: 7.0
+        score: "7.0"
     },
     {
         id: 2,
         name: "Listening",
-        score: 7.5
+        score: "7.5"
     },
     {
         id: 3,
         name: "Reading",
-        score: 7.5
+        score: "7.5"
     },
     {
         id: 4,
         name: "Writing",
-        score: 6.5
+        score: "6.5"
     },
     {
         id: 5,
         name: "Overall",
-        score: 7.0
+        score: "7.0"
     },
 ]
 
